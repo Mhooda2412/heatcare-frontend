481da5ccf1efb44368fa9a39bcc315184ecabbc3
manish kumar <manish.kumar@arthmate.com>
2024-08-13 19:18:50 +0530

    fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const cards = [
-  { name: 'Enrolment Dashboard ', imgSrc: 'line-chart.svg', path:'/line-chart' },
-  { name: 'Contract Dashboard ', imgSrc: 'bar-chart.svg', path:'/bar-chart' },
-  // { name: 'Pie Chart', imgSrc: 'pie-chart.svg', path:'/' },
-  // { name: 'Area Chart', imgSrc: 'area-chart.svg', path:'/' },
-  // { name: 'Radar Chart', imgSrc: 'radar-chart.svg', path:'/' },
-  // { name: 'Scatter Chart', imgSrc: 'scatter-chart.svg', path:'/' },
-  // { name: 'Bubble Chart', imgSrc: 'bubble-chart.svg', path:'/' },
-  // { name: 'Donut Chart', imgSrc: 'donut-chart.svg', path:'/' },
-  // { name: 'Heatmap Chart', imgSrc: 'heatmap-chart.svg', path:'/' }
+  { name: 'Enrolment Dashboard', imgSrc: 'line-chart.svg', path: '/line-chart' },
+  { name: 'Contract Dashboard', imgSrc: 'bar-chart.svg', path: '/bar-chart' },
+  // { name: 'Pie Chart', imgSrc: 'pie-chart.svg', path: '/' },
+  // { name: 'Area Chart', imgSrc: 'area-chart.svg', path: '/' },
+  // { name: 'Radar Chart', imgSrc: 'radar-chart.svg', path: '/' },
+  // { name: 'Scatter Chart', imgSrc: 'scatter-chart.svg', path: '/' },
+  // { name: 'Bubble Chart', imgSrc: 'bubble-chart.svg', path: '/' },
+  // { name: 'Donut Chart', imgSrc: 'donut-chart.svg', path: '/' },
+  // { name: 'Heatmap Chart', imgSrc: 'heatmap-chart.svg', path: '/' }
 ];
 
 const Home = () => {
@@ -20,9 +21,9 @@ const Home = () => {
           {cards.map((card, index) => (
             <div key={index} className="bg-white shadow-lg rounded-lg">
               <div className="w-full h-48 flex items-center justify-center overflow-clip">
-                  <a href={card.path}>
-                  <img src={card.imgSrc} alt={card.name} className="object-fit w-25 h-48" />
-                  </a>
+                <Link to={card.path}>
+                  <img src={card.imgSrc} alt={card.name} className="object-cover w-full h-48" />
+                </Link>
               </div>
               <div className="p-4">
                 <h2 className="text-xl font-semibold">{card.name}</h2>
